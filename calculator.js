8c880b929ca309bd5cb4aa5ce1b8a497a841954b
sirdaniel711 <66266716+sirdaniel711@users.noreply.github.com>
2020-07-07 07:56:08 -0500

    Updated to version 1.8

--- calculator.js
+++ calculator.js
@@ -2,18 +2,23 @@
 // By: sirdaniel711
 // Linked files: index.html, styles.css
 // To do:
-// Add the ability for repeated "=" clicks to reapply the current operation
 // Add support for scientific/exponential notation, along with an exponential button
+// Improve some of the code efficiency
+// Add some shortcuts (for example, pressing "6" then "x" then "=" would register as 6 x 6 = 36)
 // And more
-// Note: When pressing enter on the keyboard, if one of the buttons are currently in focus (such as from clicking on a button), then double input will occur (keyboard and focused button).
 // Note: The modulus button is currently using the symbol normally used for a percentage button (%). Normally, the symbol for a modulus button is (mod).
 
 const DIGIT_LIMIT = 15;
 let screen = document.querySelector('.display');
 let operatorScreen = document.querySelector('.display-operator');
+let memoryScreen = document.querySelector('.display-memory');
+let estimateScreen = document.querySelector('.display-estimate');
 let currentNumber = "0";
 let savedNumber = "0";
 let currentOperator = "";
+let equalsTrigger = false;
+let operatorTrigger = false;
+let estimateTrigger = false;
 document.querySelector('.calc').addEventListener('click', buttonListener);
 document.addEventListener('keydown', convertKeyboardInput);
 
@@ -31,12 +36,12 @@ function convertKeyboardInput(event) {
             break;
         case "Enter":
             request = "=";
+            clearFocus();
             break;
         case "c":
             request = "C";
             break;
     }
-    screen.focus();
     if (isNaN(parseInt(request))) {
         handleSymbol(request);
     } else {
@@ -45,6 +50,7 @@ function convertKeyboardInput(event) {
 }
 
 function buttonListener(event) {
+    clearFocus();
     if (event.target.tagName === 'BUTTON') {
         var request = event.target.innerText;
         if (isNaN(parseInt(request))) {
@@ -56,6 +62,12 @@ function buttonListener(event) {
 }
 
 function handleNumber(digit) {
+    if (equalsTrigger) {
+        reset();
+    }
+    if (operatorTrigger) {
+        operatorTrigger = false;
+    }
     if (currentNumber.length < DIGIT_LIMIT) {
         if (currentNumber === "0") {
             currentNumber = digit;
@@ -69,11 +81,7 @@ function handleNumber(digit) {
 function handleSymbol(symbol) {
     switch(symbol) {
         case "C":
-            currentNumber = "0";
-            savedNumber = "0";
-            currentOperator = "";
-            updateDisplay(currentNumber);
-            updateOperatorDisplay();
+            reset();
             break;
         case "←":
             let number = parseFloat(currentNumber);
@@ -87,6 +95,12 @@ function handleSymbol(symbol) {
             }
             break;
         case ".":
+            if (equalsTrigger) {
+                reset();
+            }
+            if (operatorTrigger) {
+                operatorTrigger = false;
+            }
             if (currentNumber.length < DIGIT_LIMIT - 1 && !currentNumber.includes(".")) {
                 currentNumber += ".";
                 updateDisplay(currentNumber);
@@ -104,11 +118,22 @@ function handleSymbol(symbol) {
             break;
         case "=":
             if (currentOperator !== "" ) {
-                currentNumber = performOperation();
-                savedNumber = "0";
-                currentOperator = "";
+                if (operatorTrigger) {
+                    operatorTrigger = false;
+                }
+                if (!equalsTrigger) {
+                    let result = performOperation();
+                    savedNumber = currentNumber;
+                    currentNumber = result;
+                    equalsTrigger = true;
+                    updateOperatorDisplay("");
+                    updateMemoryDisplay("");
+                } else {
+                    currentNumber = performOperation();
+                    updateOperatorDisplay(currentOperator);
+                    updateMemoryDisplay(savedNumber);
+                }
                 updateDisplay(currentNumber);
-                updateOperatorDisplay();
             }
             break;
         default: // default here means the case is an operator
@@ -119,17 +144,22 @@ function handleSymbol(symbol) {
 
 function handleOperator(operator) {
     if (operator === "+" || operator === "-" || operator === "×" || operator === "÷" || operator === "%") {
-        if (parseFloat(currentNumber) !== 0) {
-            savedNumber = performOperation();
+        if (operatorTrigger) {
+            currentOperator = operator;
+            updateOperatorDisplay(currentOperator);
+        } else {
+            if (equalsTrigger) {
+                savedNumber = currentNumber;
+                equalsTrigger = false;
+            } else {
+                savedNumber = performOperation();
+            }
             currentNumber = "0";
             currentOperator = operator;
-            updateDisplay(savedNumber);
-            updateOperatorDisplay();
-        } else {
-            if (currentOperator !== "") {
-                currentOperator = operator;
-                updateOperatorDisplay();
-            }
+            operatorTrigger = true;
+            updateDisplay(currentNumber);
+            updateOperatorDisplay(currentOperator);
+            updateMemoryDisplay(savedNumber);
         }
     }
 }
@@ -138,20 +168,36 @@ function performOperation() {
     let result = "0";
     switch(currentOperator) {
         case "-":
-            result = (parseFloat(savedNumber) - parseFloat(currentNumber)).toString();
+            if (equalsTrigger) {
+                result = (parseFloat(currentNumber) - parseFloat(savedNumber)).toString();
+            } else {
+                result = (parseFloat(savedNumber) - parseFloat(currentNumber)).toString();
+            }
             break;
         case "×":
             result = (parseFloat(savedNumber) * parseFloat(currentNumber)).toString();
             break;
         case "÷":
-            if (parseFloat(currentNumber) == 0) {
-                result = "DivideByZero";
+            if (equalsTrigger) {
+                if (parseFloat(savedNumber) == 0) {
+                    result = "DivideByZero";
+                } else {
+                    result = (parseFloat(currentNumber) / parseFloat(savedNumber)).toString();
+                }
             } else {
-                result = (parseFloat(savedNumber) / parseFloat(currentNumber)).toString();
+                if (parseFloat(currentNumber) == 0) {
+                    result = "DivideByZero";
+                } else {
+                    result = (parseFloat(savedNumber) / parseFloat(currentNumber)).toString();
+                }
             }
             break;
         case "%":
-            result = (parseFloat(savedNumber) % parseFloat(currentNumber)).toString();
+            if (equalsTrigger) {
+                result = (parseFloat(currentNumber) % parseFloat(savedNumber)).toString();
+            } else {
+                result = (parseFloat(savedNumber) % parseFloat(currentNumber)).toString();
+            }
             break;
         default:
             result = (parseFloat(savedNumber) + parseFloat(currentNumber)).toString();
@@ -171,13 +217,15 @@ function updateDisplay(numberToDisplay) {
         if (numberToDisplay.indexOf('.') >= currentDigitLimit || numberToDisplay.includes('e') || !numberToDisplay.includes('.')) {
             displayError("Overflow");
         } else {
-            numberToDisplay = (parseFloat(numberToDisplay).toPrecision(currentDigitLimit - 1)).toString();
+            numberToDisplay = (numberToDisplay.substring(0, currentDigitLimit));
             screen.innerText = numberToDisplay;
-            if (currentOperator === "") {
+            if (equalsTrigger) {
                 currentNumber = numberToDisplay;
             } else {
                 savedNumber = numberToDisplay;
             }
+            estimateTrigger = true;
+            updateEstimateDisplay();
         }
     } else if (numberToDisplay.includes("DivideByZero")) {
         displayError("DivideByZero");
@@ -186,16 +234,73 @@ function updateDisplay(numberToDisplay) {
     }
 }
 
-function updateOperatorDisplay() {
-    operatorScreen.innerText = currentOperator;
+function updateOperatorDisplay(operator) {
+    operatorScreen.innerText = operator;
+}
+
+function updateMemoryDisplay(numberToDisplay) {
+    var currentDigitLimit;
+    if (numberToDisplay.includes('-')) {
+        currentDigitLimit = DIGIT_LIMIT + 1;
+    } else {
+        currentDigitLimit = DIGIT_LIMIT;
+    }
+    if (numberToDisplay.length > currentDigitLimit) {
+        if (numberToDisplay.indexOf('.') >= currentDigitLimit || numberToDisplay.includes('e') || !numberToDisplay.includes('.')) {
+            displayError("Overflow");
+        } else {
+            numberToDisplay = (numberToDisplay.substring(0, currentDigitLimit));
+            memoryScreen.innerText = numberToDisplay;
+            savedNumber = numberToDisplay;
+            estimateTrigger = true;
+            updateEstimateDisplay();
+        }
+    } else if (numberToDisplay.includes("DivideByZero")) {
+        displayError("DivideByZero");
+    } else {
+        memoryScreen.innerText = numberToDisplay;
+    }
+}
+
+function updateEstimateDisplay() {
+    if (estimateTrigger) {
+        estimateScreen.innerText = "~";
+    } else {
+        estimateScreen.innerText = "";
+    }
 }
 
 function displayError(message) {
     currentNumber = "0";
     savedNumber = "0";
     currentOperator = "";
+    operatorTrigger = false;
+    equalsTrigger = false;
+    estimateTrigger = false;
     screen.innerText = "*" + message + "*";
     operatorScreen.innerText = "";
+    memoryScreen.innerText = "";
+    estimateScreen.innerText = "";
+}
+
+function reset() {
+    currentNumber = "0";
+    savedNumber = "0";
+    currentOperator = "";
+    operatorTrigger = false;
+    equalsTrigger = false;
+    estimateTrigger = false;
+    updateDisplay(currentNumber);
+    updateOperatorDisplay(currentOperator);
+    memoryScreen.innerText = "";
+    updateEstimateDisplay();
+}
+
+function clearFocus() {
+    let buttons = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "decimal", "toggle", "clear", "back", "add", "subtract", "multiply", "divide", "modulus", "equal"];
+    for (let i = 0; i < buttons.length; i++) {
+        document.querySelector('.' + buttons[i]).blur();
+    }
 }
 
 // Version 1.0
@@ -215,7 +320,13 @@ function displayError(message) {
 //   -The overflow error message would not display for full numbers that are too large to fit on the display
 //   -Hitting backspace or the back button on a decimal number would make it 0 instead of removing the last digit
 // Version 1.6
-// *Added a decimal button, modulus button, and toggle button for switching a number between positive/negative.
+// *Added a decimal button, modulus button, and toggle button for switching a number between positive/negative
 // Version 1.7
-// *Fixed a few errors where the backspace would not work properly.
-// *Updated the button layout.
+// *Fixed a few errors where the backspace would not work properly
+// *Updated the button layout
+// Version 1.8
+// *Added the ability for repeated "=" clicks to reapply the current operation
+// *Added a display for the approximate symbol "~" to show whenever a number is estimated to 15 digits due to screen space limitations
+// *Added a display for the number stored in memory
+// *Fixed an issue where small decimal numbers were not rounding correctly to 15 digits
+// *Fixed the issue with the buttons being in focus
